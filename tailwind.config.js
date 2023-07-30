5c6d08b73fc9d0b8df335b2987bfca3e546ef333
Khang Tran <ktra@email.unc.edu>
2023-07-29 21:42:30 -0400

    added custom font, colors, and font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "inter-bold": ["Inter-Bold"],
+        "inter-extrabold": ["Inter-ExtraBold"],
+        "inter-regular": ["Inter-Regular"],
+      },
+      colors: {
+        "soft-orange": "hsl(35, 77%, 62%)",
+        "soft-red": "hsl(5, 85%, 63%)",
+        "off-white": "hsl(36, 100%, 99%)",
+        "gray-blue": "hsl(233, 8%, 79%)",
+        "dark-gray-blue": "hsl(236, 13%, 42%)",
+        "dark-blue": "hsl(240, 100%, 5%)",
+        blueish: "hsl(228, 44.90%, 44.10%)",
+      },
+      fontSize: {
+        footer: "11px",
+        body: "15px",
+      },
+    },
   },
   plugins: [],
 };
