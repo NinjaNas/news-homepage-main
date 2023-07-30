add34f20bd60d311d935cbe8ce315abc2daafeb2
Khang Tran <ktra@email.unc.edu>
2023-07-29 22:21:23 -0400

    reduce margin on xl screens

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 
 function App(props: { children: JSXElement | JSXElement[] }) {
 	return (
-		<div class="flex min-h-screen flex-col justify-around p-5 md:mx-[10%] xl:mx-[20%]">
+		<div class="flex min-h-screen flex-col justify-around p-5 md:mx-[10%] xl:mx-[15%]">
 			<Navbar tabsArr={["Home", "New", "Popular", "Trending", "Categories"]}></Navbar>
 			{props.children}
 			<Footer></Footer>
